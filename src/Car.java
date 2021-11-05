b1c8cf9fc643f3ed96d9362c26c47dd89cf3042f
n1ghtshade <scs.bon7@gmail.com>
2021-11-06 00:40:24 +0300

    haa

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,74 @@
+import java.util.Objects;
+
 public class Car {
-    int maxSpeed;
-    int weight;
-    double volume;
-    String name;
+
+    private int maxSpeed;
+    private int weight;
+    private double volume;
+    private String VIN;
+    private String name;
+
+    public Car(int maxSpeed, int weight, double volume, String VIN, String name) {
+        this.maxSpeed = maxSpeed;
+        this.weight = weight;
+        this.volume = volume;
+        this.VIN = VIN;
+        this.name = name;
+    }
 
 
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return VIN.equals(car.VIN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(VIN);
+    }
 }
+
--- src/MainCar.java
+++ src/MainCar.java
@@ -1,28 +1,18 @@
 public class MainCar {
 
     public static void main(String[] args) {
-        Car car1 = new Car();
-        Car car2 = new Car();
-        boolean compare;
-        car1.maxSpeed = 270;
-        car1.weight = 1195;
-        car1.volume = 1.8;
-        car1.name = "какая-то ламба";
-        car2.maxSpeed = 300;
-        car2.weight = 950;
-        car2.volume = 1.8;
-        car2.name = "какая-то феррари";
-        car1 = car2;
+        Car car1 = new Car(260,2000,2.7,"123RT456","BMW");
+        Car car2 = new Car(250,2500,2.7,"123RT4567","Audi");
 
-
-        compare = car1.equals(car2);
-        System.out.println("Машины одинаковы? "+compare);
-        if (car1.maxSpeed> car2.maxSpeed || car1.volume>= car2.volume){
-            System.out.println("Вероятнее всего "+car1.name+" выйдет победителем");
+        System.out.println("Машины одинаковы? "+car1.equals(car2));
+        if (car1.getMaxSpeed()> car2.getMaxSpeed() || car1.getVolume()>= car2.getVolume()){
+            System.out.println("Вероятнее всего "+car1.getName()+" выйдет победителем");
         }
         else {
-            System.out.println("Вероятнее всего "+car2.name+" выйдет победителем");
+            System.out.println("Вероятнее всего "+car2.getName()+" выйдет победителем");
         }
+        
+
 
     }
 }
